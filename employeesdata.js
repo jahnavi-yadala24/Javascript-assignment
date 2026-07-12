4a0516c9f41cd6cf3d0213e91892955b5c7d0875
jahnavi-yadala24 <yadalajahnavi24@gmail.com>
2026-07-12 15:42:02 +0530

    Update employeesdata.js

--- employeesdata.js
+++ employeesdata.js
@@ -2,27 +2,42 @@ const employees = [
     {
         name: "Harshitha",
         salary: 1500000,
-        projectAllocated: false
+        experience: 4,
+        track: 'engineering',
+        projectAllocated: false,
+        projectCompletionDate: null
     },
     {
         name: "Mokshitha",
         salary: 1800000,
-        projectAllocated: false
+        experience: 3,
+        track: 'engineering',
+        projectAllocated: false,
+        projectCompletionDate: "2026/06/21"
     },
     {
         name: "Nikitha",
         salary: 1800000,
-        projectAllocated: true
+        experience: 5,
+        track: 'engineering',
+        projectAllocated: true,
+        projectCompletionDate: "2026/05/26"
     },
     {
         name: "Jahnavi",
         salary: 1700000,
-        projectAllocated: true
+        experience: 4,
+        track: 'engineering',
+        projectAllocated: true,
+        projectCompletionDate: "2026/03/12"
     },
     {
         name: "Sushma",
         salary: 1200000,
-        projectAllocated: false
+        experience: 3,
+        track: 'engineering',
+        projectAllocated: false,
+        projectCompletionDate: null
     }
 ];
 
